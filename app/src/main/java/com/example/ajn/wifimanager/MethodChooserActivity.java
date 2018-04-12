575fa907a4565a76fb87d09a970a22459a65cbf2
madhavsankar <madhavsankar@gmail.com>
2018-04-13 00:34:23 +0530

    Add Fingerprint Authentication

--- app/src/main/java/com/example/ajn/wifimanager/MethodChooserActivity.java
+++ app/src/main/java/com/example/ajn/wifimanager/MethodChooserActivity.java
@@ -5,8 +5,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.Manifest;
+import android.os.Build;
+import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 public class MethodChooserActivity extends AppCompatActivity {
+    private static final String KEY_NAME = "yourKey";
+    private Cipher cipher;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
+    private TextView textView;
+    private FingerprintManager.CryptoObject cryptoObject;
+    private FingerprintManager fingerprintManager;
+    private KeyguardManager keyguardManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +56,57 @@ public class MethodChooserActivity extends AppCompatActivity {
         generator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MethodChooserActivity.this,QRGeneratorActivity.class));
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    //Get an instance of KeyguardManager and FingerprintManager//
+                    keyguardManager =
+                            (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+                    fingerprintManager =
+                            (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+
+                    //textView = (TextView) findViewById(R.id.textview);
+
+                    //Check whether the device has a fingerprint sensor//
+                    if (!fingerprintManager.isHardwareDetected()) {
+                        // If a fingerprint sensor isn’t available, then inform the user that they’ll be unable to use your app’s fingerprint functionality//
+                        //textView.setText("Your device doesn't support fingerprint authentication");
+                    }
+                    //Check whether the user has granted your app the USE_FINGERPRINT permission//
+                    if (ActivityCompat.checkSelfPermission(MethodChooserActivity.this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                        // If your app doesn't have this permission, then display the following text//
+                        //textView.setText("Please enable the fingerprint permission");
+                    }
+
+                    //Check that the user has registered at least one fingerprint//
+                    if (!fingerprintManager.hasEnrolledFingerprints()) {
+                        // If the user hasn’t configured any fingerprints, then display the following message//
+                        //textView.setText("No fingerprint configured. Please register at least one fingerprint in your device's Settings");
+                    }
+
+                    //Check that the lockscreen is secured//
+                    if (!keyguardManager.isKeyguardSecure()) {
+                        // If the user hasn’t secured their lockscreen with a PIN password or pattern, then display the following text//
+                        //textView.setText("Please enable lockscreen security in your device's Settings");
+                    } else {
+                        try {
+                            generateKey();
+                        } catch (FingerprintException e) {
+                            e.printStackTrace();
+                        }
+
+                        if (initCipher()) {
+                            //If the cipher is initialized successfully, then create a CryptoObject instance//
+                            cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                            // Here, I’m referencing the FingerprintHandler class that we’ll create in the next section. This class will be responsible
+                            // for starting the authentication process (via the startAuth method) and processing the authentication process events//
+                            FingerprintHandler helper = new FingerprintHandler(MethodChooserActivity.this);
+                            helper.startAuth(fingerprintManager, cryptoObject);
+                        }
+                    }
+                }
+
+
+                //startActivity(new Intent(MethodChooserActivity.this,QRGeneratorActivity.class));
             }
         });
 
@@ -28,5 +116,79 @@ public class MethodChooserActivity extends AppCompatActivity {
                 startActivity(new Intent(MethodChooserActivity.this,ScannerActivity.class));
             }
         });
+    }
+
+    private void generateKey() throws FingerprintException {
+        try {
+
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+            keyStore.load(null);
+
+            keyGenerator.init(new
+
+
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+
+            keyGenerator.generateKey();
+
+        } catch (KeyStoreException
+                | NoSuchAlgorithmException
+                | NoSuchProviderException
+                | InvalidAlgorithmParameterException
+                | CertificateException
+                | IOException exc) {
+            exc.printStackTrace();
+            throw new FingerprintException(exc);
+        }
+    }
+
+    public boolean initCipher() {
+        try {
+            cipher = Cipher.getInstance(
+                    KeyProperties.KEY_ALGORITHM_AES + "/"
+                            + KeyProperties.BLOCK_MODE_CBC + "/"
+                            + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException |
+                NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
+                    null);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+        } catch (KeyPermanentlyInvalidatedException e) {
+
+            return false;
+        } catch (KeyStoreException | CertificateException
+                | UnrecoverableKeyException | IOException
+                | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+    }
+
+    private class FingerprintException extends Exception {
+        public FingerprintException(Exception e) {
+            super(e);
+        }
+    }
+
+    public static void foo(Context context) {
+        Intent intent = new Intent(context,QRGeneratorActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 }
